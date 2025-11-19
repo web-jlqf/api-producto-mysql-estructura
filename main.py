cf287971f2a74a6889bb849dceb7622fcb7c1806
José Luis <luis.quiroz@uacm.edu.mx>
2025-11-19 07:57:32 -0600

    Agregando cors

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 # main.py
 from fastapi import FastAPI
 from routers.product_router import router as product_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="API REST  - Productos")
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],  # Permitir todos los encabezados
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 app.include_router(product_router)
 
